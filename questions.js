32cfa4b685ec955a106984eae844b450403c7f88
phanhiep1982 <phanhiep01011982@gmail.com>
2026-03-08 20:53:59 +0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,8 +1,10 @@
 const fullBank = [
     { type: 'match', q: "Happy", a: "😊", options: ["😊", "😢", "😡", "😴"], sound: "Happy" },
-    { type: 'quiz', q: "Thanh Vân ơi, What is this?", img: "🛌", a: "Bedroom", options: ["Kitchen", "Bedroom", "Yard", "Hall"], sound: "Bedroom" },
+    { type: 'quiz', q: "Thanh Vân ơi, what is this?", img: "🛌", a: "Bedroom", options: ["Kitchen", "Bedroom", "Yard", "Hall"], sound: "Bedroom" },
     { type: 'scramble', q: "I want noodles", a: "I want noodles", parts: ["I", "want", "noodles"], sound: "I want noodles" },
     { type: 'tf', q: "Rainy", img: "🌧️", a: true, sound: "Rainy" },
-    { type: 'quiz', q: "Can you use chopsticks?", a: "Can you use chopsticks?", options: ["use chopsticks?", "eat fruit?", "drink juice?", "want fish?"], sound: "Can you use chopsticks?" }
+    { type: 'quiz', q: "Can you use chopsticks?", a: "Can you use chopsticks?", options: ["use chopsticks?", "eat fruit?", "drink juice?", "want fish?"], sound: "Can you use chopsticks?" },
+    { type: 'match', q: "Sushi (Japan)", a: "🍣", options: ["🍣", "🥟", "🌮", "🌭"], sound: "Sushi" },
+    { type: 'quiz', q: "Where is Tacos from?", img: "🌮", a: "Mexico", options: ["China", "Japan", "Korea", "Mexico"], sound: "Mexico" },
+    { type: 'tf', q: "Judo practice", img: "🥋", a: true, sound: "Judo practice" }
 ];
-// Đảm bảo có dấu chấm phẩy ở cuối cùng này anh nhé!
